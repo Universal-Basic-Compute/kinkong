33b90df14393db0771235bee2794228ff30c0c73
Lesterpaintstheworld <reynolds.nicorr@gmail.com>
2025-02-14 22:35:49 +0100

    src

--- app/providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { SolanaWalletProvider } from '@/components/wallet/WalletProvider';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <SolanaWalletProvider>{children}</SolanaWalletProvider>;
-}
